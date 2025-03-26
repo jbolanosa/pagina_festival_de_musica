b0b9e02b89490463856e79ad53302947d71e21fa
José Bolaños <84158344+jbolanosa@users.noreply.github.com>
2025-03-25 20:54:32 -0600

    agregando imagenes optimizadas

--- src/js/app.js
+++ src/js/app.js
@@ -23,12 +23,12 @@ function crearGaleria() {
     const galeria = document.querySelector('.galeria-imagenes')
 
     for(let i = 1; i <= CANTIDAD_IMAGENES; i++){
-        const imagen = document.createElement('IMG')
-        imagen.loading = 'lazy'
-        imagen.width = '300'
-        imagen.height = '200'
-        imagen.src =  `src/img/gallery/thumb/${i}.jpg`
-        imagen.alt = 'Imagen Galería'
+        const imagen = document.createElement('PICTURE')
+        imagen.innerHTML = `
+        <source srcset="build/img/gallery/thumb/${i}.avif" type="image/avif">
+        <source srcset="build/img/gallery/thumb/${i}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/gallery/thumb/${i}.jpg" alt="imagen galeria">
+         `;
 
 
         //Event handler
@@ -41,9 +41,12 @@ function crearGaleria() {
 }
 
 function mostrarImagen(i) {
-    const imagen = document.createElement('IMG')
-    imagen.src =  `src/img/gallery/full/${i}.jpg`
-    imagen.alt = 'Imagen Galería'
+    const imagen = document.createElement('PICTURE')
+    imagen.innerHTML = `
+        <source srcset="build/img/gallery/full/${i}.avif" type="image/avif">
+        <source srcset="build/img/gallery/full/${i}.webp" type="image/webp">
+        <img loading="lazy" width="200" height="300" src="build/img/gallery/full/${i}.jpg" alt="imagen galeria">
+    `;
 
     //generar modal
     const modal = document.createElement('DIV')
